4952b58abc2a67e3c719e6c4d8ae8b0b7c7c214e
Michal Samulak <sanchossjmistik@gmail.com>
2021-03-15 13:17:02 +0000

    event hanlding for rolle dice

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,29 @@ import '../scss/main.scss';
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice)
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector(`#current--${activePlayer}`).textContent = dice;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '1';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '1';
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+   
+    // 1. Ranom number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = `/public/img/dice-${dice}.png`;
+
+
+
+    // 3. Update the round score IF the roller number was NOT a 1
+
+})
