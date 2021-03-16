94a99b7bac97e8ec7ca731af8b3c8c5856efbad2
Michal Samulak <sanchossjmistik@gmail.com>
2021-03-16 12:49:00 +0000

    init function for start-restart game

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,7 @@ import '../scss/main.scss';
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
 
@@ -61,7 +52,7 @@ document.querySelector('.btn-hold').addEventListener('click', () => {
 
 })
 
-const nextPlayer = () => {
+function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
@@ -73,3 +64,31 @@ const nextPlayer = () => {
 
     document.querySelector('.dice').style.display = 'none';
 }
+
+
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('player-0-panel').classList.remove('winner');
+    document.querySelector('player-1-panel').classList.remove('winner');
+    document.querySelector('player-0-panel').classList.remove('active');
+    document.querySelector('player-1-panel').classList.remove('active');
+    document.querySelector('player-0-panel').classList.add('active');
+
+
+
+}
+
